758ff0fe1f0fca1bee35dbf83561a24fc7fcc8fb
mrfaridnaz <mrfad8392@gmail.com>
2024-08-04 17:38:14 +0530

    added

--- main.py
+++ main.py
@@ -1,23 +1,30 @@
-from mlProject import logger
-from mlProject.pipeline.stafe_01_data_ingestion import DataIngestionTrainingPipeline
-from mlProject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+from mlProject.constants import *
+from mlProject.utils.common import read_yaml, create_directories
+from mlProject.entity.config_entity import DataIngestionConfig
 
-STAGE_NAME = "Data Ingestion stage"
-try:
-   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-   data_ingestion = DataIngestionTrainingPipeline()
-   data_ingestion.main()
-   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
-except Exception as e:
-        logger.exception(e)
-        raise e
 
-STAGE_NAME = "Data Validation stage"
-try:
-   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-   data_ingestion = DataValidationTrainingPipeline()
-   data_ingestion.main()
-   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
-except Exception as e:
-        logger.exception(e)
-        raise e
+class ConfigurationManager:
+    def __init__(
+            self,
+            config_filepath=CONFIG_FILE_PATH,
+            params_filepath=PARAMS_FILE_PATH,
+            schema_filepath=SCHEMA_FILE_PATH):
+        self.config = read_yaml(config_filepath)
+        self.params = read_yaml(params_filepath)
+        self.schema = read_yaml(schema_filepath)
+
+        create_directories([self.config.artifacts_root])
+
+    def get_data_ingestion_config(self) -> DataIngestionConfig:
+        config = self.config.data_ingestion
+
+        create_directories([config.root_dir])
+
+        data_ingestion_config = DataIngestionConfig(
+            root_dir=config.root_dir,
+            source_URL=config.source_URL,
+            local_data_file=config.local_data_file,
+            unzip_dir=config.unzip_dir
+        )
+
+        return data_ingestion_config
